e89318b38db20e88f8039972c5f6bfa3d789be3b
Miiko <333miiko@gmail.com>
2017-11-06 17:24:29 +1100

    Link secnode config

--- secnodetracker/entrypoint.sh
+++ secnodetracker/entrypoint.sh
@@ -3,7 +3,7 @@
 # link the secure node tracker config, bail if not present
 if [ -f "/mnt/zen/secnode/stakeaddr" ]; then
   echo "Secure node config found OK - linking..."
-  cp -r /mnt/zen/secnode /home/node/secnodetracker/config > /dev/null 2>&1 || true
+  ln -s /mnt/zen/secnode /home/node/secnodetracker/config > /dev/null 2>&1 || true
 else
   echo "No secure node config found. exiting"
   exit 1

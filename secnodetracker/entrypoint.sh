0eba2eae80125af631f51a21cbb2a855766e1ce2
Miiko <333miiko@gmail.com>
2017-11-14 11:14:04 +1100

    Fix permissions and zcash-params

--- secnodetracker/entrypoint.sh
+++ secnodetracker/entrypoint.sh
@@ -3,14 +3,17 @@
 # link the secure node tracker config, bail if not present
 if [ -f "/mnt/zen/secnode/stakeaddr" ]; then
   echo "Secure node config found OK - linking..."
-  ln -s /mnt/zen/secnode /home/node/secnodetracker/config > /dev/null 2>&1 || true
+  ln -s /mnt/zen/secnode /home/user/secnodetracker/config > /dev/null 2>&1 || true
 else
   echo "No secure node config found. exiting"
   exit 1
 fi
 
-# Fix ownership of the created files/folders
-chown -R node:node /home/node /mnt/zen /mnt/zcash-params
+# Copy the zencash params
+cp -r /mnt/zen/zcash-params /mnt/zcash-params
+
+# Fix the permissons
+chown -R user:user /home/node/secnodetracker/config /mnt/zcash-params
 
 cd secnodetracker
 gosu node node app.js

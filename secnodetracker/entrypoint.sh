573ae5f42a13cbdf10d14c1d962adfd79503573b
Miiko <333miiko@gmail.com>
2018-07-19 03:49:09 +0000

    Fix new secnodetracker config

--- secnodetracker/entrypoint.sh
+++ secnodetracker/entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 # link the secure node tracker config, bail if not present
-if [ -f "/mnt/zen/secnode/stakeaddr" ]; then
+if [ -f "/mnt/zen/secnode/config.json" ]; then
   echo "Secure node config found OK - linking..."
   ln -s /mnt/zen/secnode /home/node/secnodetracker/config > /dev/null 2>&1 || true
 else
   echo "No secure node config found. exiting"
   exit 1
-fi
-
-# Fix for new ipv param
-if [ ! -f "/mnt/zen/secnode/ipv" ]; then
-  echo "4" > /mnt/zen/secnode/ipv
 fi
 
 # Copy the zencash params
